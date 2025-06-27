29c8612ab741b4ce1ddbd6c45082eb30b2d60252
Sancho, Andres <asancho+deloitte@deloitte.es>
2025-06-27 13:18:05 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Auth, GoogleAuthProvider } from '@angular/fire/auth';
+import { ApiService } from './services/api.service';
+import { signInWithPopup } from 'firebase/auth';
+import { MapComponent } from './components/map/map';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  template: `
+    <div class="p-4">
+      @if(loggedIn()) {
+      <app-map />
+      }
+    </div>
+  `,
+  imports: [MapComponent],
 })
-export class App {
-  protected title = 'trainer-map';
+export class AppComponent {
+  private auth = inject(Auth);
+  private api = inject(ApiService);
+
+  loggedIn = signal(false);
+
+  ngOnInit() {
+    this.loginWithGoogle();
+  }
+
+  async loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(this.auth, provider);
+      this.loggedIn.set(true);
+    } catch (error: any) {
+      this.loggedIn.set(false);
+      console.error('Error al iniciar sesión con Google:', error);
+    }
+  }
+  async callApi() {
+    try {
+      const res: any = await this.api.getProtectedData();
+    } catch (err: any) {
+      console.log(err.error || 'Error al llamar API');
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB1lGQiPcBL-OSpb35rtdYwMX11sAjkAQk',
+    authDomain: 'trainer-map-3be6b.firebaseapp.com',
+    projectId: 'trainer-map-3be6b',
+    storageBucket: 'trainer-map-3be6b.firebasestorage.app',
+    messagingSenderId: '348167935629',
+    appId: '1:348167935629:web:045539482f8bd81786b4c4',
+    measurementId: 'G-Q6PP5XTK5X',
+  },
+  baseUrl: 'http://localhost:3000/api',
+};
