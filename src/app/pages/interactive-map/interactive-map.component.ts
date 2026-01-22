f2e7eb5a832d79d8e76ef1b225016e04d5e036f9
Andrés Sancho <asancho.95.tr@gmail.com>
2026-01-22 15:14:46 +0100

    feat: Mensajes de pruebas

--- src/app/pages/interactive-map/interactive-map.component.ts
+++ src/app/pages/interactive-map/interactive-map.component.ts
@@ -36,7 +36,7 @@ export class InteractiveMapComponent {
     {
       id: '5',
       x: 20,
-      y: 16,
+      y: 16,//Combate pokémon
       info: 'En Paldea aprendí que no basta con tener criaturas fuertes, hay que tener estrategia. ¿Te atreves a medirte conmigo en un verdadero combate Pokémon?',//Combate pokémon
       characters: [TRAINERS.ANDRES],
       badge: BADGES.BADGE_5,
@@ -45,8 +45,8 @@ export class InteractiveMapComponent {
     {
       id: '2',
       x: 76.5,
-      y: 90.5,
-      info: 'Párrafo con información de la prueba',
+      y: 90.5,//Componer y cantar una canción a lo HSM
+      info: 'En este gimnasio no basta con fuerza ni estrategia… aquí el corazón marca el ritmo. Si puedes cantar lo que sientes y hacerlo brillar como en un musical, quizá estés listo para continuar.',
       characters: [TRAINERS.SOF, TRAINERS.SIL, TRAINERS.LUIS],
       badge: BADGES.BADGE_2,
       color: 'aqua'
@@ -54,17 +54,17 @@ export class InteractiveMapComponent {
     {
       id: '7',
       x: 25.5,
-      y: 27,
+      y: 27,//Foto a redes que llegue a X likes
       info: 'El eco de tu triunfo no siempre se escucha en el estadio… a veces se mide en aplausos invisibles. Haz que tu imagen conquiste a la multitud.',
-      characters: [TRAINERS.ANDREA, TRAINERS.JAIME, TRAINERS.SORAYA],//Foto a redes que llegue a X likes
+      characters: [TRAINERS.ANDREA, TRAINERS.JAIME, TRAINERS.SORAYA],
       badge: BADGES.BADGE_7,
       color: 'red'
     },
     {
       id: '4',
       x: 23.5,
-      y: 32,
-      info: 'Párrafo con información de la prueba',
+      y: 32,//Salir a tomar café en pijama
+      info: 'Los verdaderos entrenadores no temen al qué dirán. A veces, el valor se demuestra saliendo al mundo tal y como eres… incluso antes de cambiarte de ropa.',
       characters: [TRAINERS.CLARA, TRAINERS.SERGIO],
       badge: BADGES.BADGE_4,
       color: 'green'
@@ -79,7 +79,7 @@ export class InteractiveMapComponent {
     {
       id: '3',
       x: 40.5,
-      y: 28,
+      y: 28,//Baby shark con la guitarra
       // info: 'La melodía de la victoria no siempre es heróica. Incluso un canto infantil puede rugir como un trueno si lo tocas con cuerdas de acero.',//Baby shark con la guitarra
       info: 'Bienvenidos al gimnasio Aguilar. Todas las salidas están selladas y solo tocar la canción mágica disipará la maldición y evitará que seais deborados por los temibles tiburones de la estanca.',
       characters: [TRAINERS.RU],
@@ -98,8 +98,8 @@ export class InteractiveMapComponent {
     {
       id: '1',
       x: 77.2,
-      y: 26.2,
-      info: 'Párrafo con información de la prueba',
+      y: 26.2,//Trivial de geología
+      info: 'El mundo se construyó capa a capa, y solo quienes conocen su historia pueden avanzar. ¿Sabrás distinguir lo que es roca sólida de lo que se desmorona al primer temblor?',
       characters: [TRAINERS.LU],
       badge: BADGES.BADGE_1,
       color: 'brown'
@@ -110,7 +110,7 @@ export class InteractiveMapComponent {
       y: 42.5,
       info: 'Dicen que para alcanzar la gloria basta con dar en el blanco, no siempre en el corazón. Apunta firme… aunque no seas Robin Hood.',//Tiro con arco
       characters: [TRAINERS.NOAH],
-      badge: BADGES.BADGE_8,
+      badge: BADGES.BADGE_8,//Tiro con arco
       color: 'purple'
     },
   ].map((marker) => { 
